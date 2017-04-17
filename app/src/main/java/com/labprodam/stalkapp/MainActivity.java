043f16552c67eb17ef51f44512c23384a731c430
danielfsilva88 <daniel.ferreira.silva88@gmail.com>
2017-04-17 18:16:49 -0300

    1st--creating-Stalkapp

---
+++ app/src/main/java/com/labprodam/stalkapp/MainActivity.java
@@ -0,0 +1,225 @@
+package com.labprodam.stalkapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
+
+    private double mLat, mLon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String id = Installation.id(this); // get appUser ID
+
+        // creates an instance (mSensorManager) to access sensors
+        SensorManager mSensorManager = ( SensorManager ) getSystemService( Context.SENSOR_SERVICE );
+        // accesses accelerometer sensor
+        Sensor mAcc = mSensorManager.getDefaultSensor( Sensor.TYPE_ACCELEROMETER );
+        // controls the interval at which sensor events are sent to your application via the onSensorChanged() callback method (SENSOR_DELAY_FASTEST has 0 ms delay)
+        mSensorManager.registerListener( this, mAcc, SensorManager.SENSOR_DELAY_FASTEST );
+
+/* Teste para printar na tela do app o num de sensores e algumas das suas infos. Para obter outras infos:
+https://developer.android.com/guide/topics/sensors/sensors_overview.html
+        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        TextView coord = (TextView) findViewById(R.id.Coords);
+        int nsensors = deviceSensors.size();
+        String[] name = new String[nsensors], vendor = new String[nsensors];
+        String[] it = new String[nsensors], version = new String[nsensors];
+
+        for (int i = 0; i < nsensors; i++){
+            name[i] = deviceSensors.get(i).getName();
+            vendor[i] = deviceSensors.get(i).getVendor();
+            it[i] = String.valueOf(i);
+            version[i] = String.valueOf(deviceSensors.get(i).getVersion());
+        }
+        coord.setText( "Number of Sensors = " + nsensors + ";\n\n" +
+        "- Sensor Name " + it[0] + ": " + name[0] + "; Sensor vendor: " + vendor[0] + "; Sensor version: " + version[0] + ";\n\n" +
+        "- Sensor Name " + it[1] + ": " + name[1] + "; Sensor vendor: " + vendor[1] + "; Sensor version: " + version[1] + ";\n\n" +
+        "- Sensor Name " + it[2] + ": " + name[2] + "; Sensor vendor: " + vendor[2] + "; Sensor version: " + version[2] + ";\n\n"
+        );
+// até aqui o código funciona no Android 7.0 - MotoG5 possui 21 sensores
+    */
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            return;
+        }
+
+        // getting last known GPS coordinates - if coords changes, function onLocationChanged is autocalled
+        /*Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        String lat = String.format(Locale.getDefault(), "%.2f", loc.getLatitude());
+        String lon = String.format(Locale.getDefault(), "%.2f", loc.getLongitude());
+        TextView coord = (TextView) findViewById(R.id.Coords);
+        coord.setText( "Latitude = " + lat + "º;\n" + "Longitude = " + lon + "º.");
+*/
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, (LocationListener) this);
+
+    }
+
+    public static class Installation {
+        private static String sID = null;
+        private static final String INSTALLATION = "INSTALLATION";
+
+        public synchronized static String id (Context context) {
+            if (sID == null) {
+                File installation = new File(context.getFilesDir(), INSTALLATION);
+                try {
+                    if (!installation.exists())
+                        writeInstallationFile(installation);
+                    sID = readInstallationFile(installation);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return sID;
+        }
+
+        private static String readInstallationFile(File installation) throws IOException {
+            RandomAccessFile f = new RandomAccessFile(installation, "r");
+            byte[] bytes = new byte[(int) f.length()];
+            f.readFully(bytes);
+            f.close();
+            return new String(bytes);
+        }
+
+        private static void writeInstallationFile(File installation) throws IOException {
+            FileOutputStream out = new FileOutputStream(installation);
+            String id = UUID.randomUUID().toString();
+            out.write(id.getBytes());
+            out.close();
+        }
+    }
+
+    //@Override
+    public void onLocationChanged(Location location) {
+        mLat = location.getLatitude();
+        mLon = location.getLongitude();
+        //TextView coord = (TextView) findViewById(R.id.Coords);
+        //coord.setText( "Latitude = " + mLat + ";" + "Longitude = " + mLon );
+    }
+
+    // Functions created when declared "implements SensorEventListener, LocationListener"
+    /**
+     * Called when there is a new sensor event.  Note that "on changed"
+     * is somewhat of a misnomer, as this will also be called if we have a
+     * new reading from a sensor with the exact same sensor values (but a
+     * newer timestamp).
+     * <p>
+     * <p>See {@link SensorManager SensorManager}
+     * for details on possible sensor types.
+     * <p>See also {@link SensorEvent SensorEvent}.
+     * <p>
+     * <p><b>NOTE:</b> The application doesn't own the
+     * {@link SensorEvent event}
+     * object passed as a parameter and therefore cannot hold on to it.
+     * The object may be part of an internal pool and may be reused by
+     * the framework.
+     *
+     * @param event the {@link SensorEvent SensorEvent}.
+     */
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    /**
+     * Called when the accuracy of the registered sensor has changed.  Unlike
+     * onSensorChanged(), this is only called when this accuracy value changes.
+     * <p>
+     * <p>See the SENSOR_STATUS_* constants in
+     * {@link SensorManager SensorManager} for details.
+     *
+     * @param sensor
+     * @param accuracy The new accuracy of this sensor, one of
+     *                 {@code SensorManager.SENSOR_STATUS_*}
+     */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    /**
+     * Called when the provider status changes. This method is called when
+     * a provider is unable to fetch a location or if the provider has recently
+     * become available after a period of unavailability.
+     *
+     * @param provider the name of the location provider associated with this
+     *                 update.
+     * @param status   {@link LocationProvider#OUT_OF_SERVICE} if the
+     *                 provider is out of service, and this is not expected to change in the
+     *                 near future; {@link LocationProvider#TEMPORARILY_UNAVAILABLE} if
+     *                 the provider is temporarily unavailable but is expected to be available
+     *                 shortly; and {@link LocationProvider#AVAILABLE} if the
+     *                 provider is currently available.
+     * @param extras   an optional Bundle which will contain provider specific
+     *                 status variables.
+     *                 <p>
+     *                 <p> A number of common key/value pairs for the extras Bundle are listed
+     *                 below. Providers that use any of the keys on this list must
+     *                 provide the corresponding value as described below.
+     *                 <p>
+     *                 <ul>
+     *                 <li> satellites - the number of satellites used to derive the fix
+     */
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    /**
+     * Called when the provider is enabled by the user.
+     *
+     * @param provider the name of the location provider associated with this
+     *                 update.
+     */
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    /**
+     * Called when the provider is disabled by the user. If requestLocationUpdates
+     * is called on an already disabled provider, this method is called
+     * immediately.
+     *
+     * @param provider the name of the location provider associated with this
+     *                 update.
+     */
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
