d406f9a3294c92c0959e1220d454d8cc51c34a7a
danielfsilva88 <daniel.ferreira.silva88@gmail.com>
2017-04-17 19:00:11 -0300

    trying to get sensors data

--- app/src/main/java/com/labprodam/stalkapp/MainActivity.java
+++ app/src/main/java/com/labprodam/stalkapp/MainActivity.java
@@ -11,27 +11,41 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
+import android.net.ConnectivityManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
 
+
     private double mLat, mLon;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String id = Installation.id(this); // get appUser ID
+        mContext = this.getApplicationContext();
+
+        //String id = Installation.id(this); // get appUser ID
 
         // creates an instance (mSensorManager) to access sensors
         SensorManager mSensorManager = ( SensorManager ) getSystemService( Context.SENSOR_SERVICE );
@@ -39,28 +53,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Sensor mAcc = mSensorManager.getDefaultSensor( Sensor.TYPE_ACCELEROMETER );
         // controls the interval at which sensor events are sent to your application via the onSensorChanged() callback method (SENSOR_DELAY_FASTEST has 0 ms delay)
         mSensorManager.registerListener( this, mAcc, SensorManager.SENSOR_DELAY_FASTEST );
-
-/* Teste para printar na tela do app o num de sensores e algumas das suas infos. Para obter outras infos:
-https://developer.android.com/guide/topics/sensors/sensors_overview.html
-        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-        TextView coord = (TextView) findViewById(R.id.Coords);
-        int nsensors = deviceSensors.size();
-        String[] name = new String[nsensors], vendor = new String[nsensors];
-        String[] it = new String[nsensors], version = new String[nsensors];
-
-        for (int i = 0; i < nsensors; i++){
-            name[i] = deviceSensors.get(i).getName();
-            vendor[i] = deviceSensors.get(i).getVendor();
-            it[i] = String.valueOf(i);
-            version[i] = String.valueOf(deviceSensors.get(i).getVersion());
-        }
-        coord.setText( "Number of Sensors = " + nsensors + ";\n\n" +
-        "- Sensor Name " + it[0] + ": " + name[0] + "; Sensor vendor: " + vendor[0] + "; Sensor version: " + version[0] + ";\n\n" +
-        "- Sensor Name " + it[1] + ": " + name[1] + "; Sensor vendor: " + vendor[1] + "; Sensor version: " + version[1] + ";\n\n" +
-        "- Sensor Name " + it[2] + ": " + name[2] + "; Sensor vendor: " + vendor[2] + "; Sensor version: " + version[2] + ";\n\n"
-        );
-// até aqui o código funciona no Android 7.0 - MotoG5 possui 21 sensores
-    */
 
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
@@ -79,21 +71,22 @@ https://developer.android.com/guide/topics/sensors/sensors_overview.html
         }
 
         // getting last known GPS coordinates - if coords changes, function onLocationChanged is autocalled
-        /*Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        ///*
+        Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         String lat = String.format(Locale.getDefault(), "%.2f", loc.getLatitude());
         String lon = String.format(Locale.getDefault(), "%.2f", loc.getLongitude());
-        TextView coord = (TextView) findViewById(R.id.Coords);
-        coord.setText( "Latitude = " + lat + "º;\n" + "Longitude = " + lon + "º.");
-*/
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, (LocationListener) this);
+        //TextView coord = (TextView) findViewById(R.id.Coords);
+        //coord.setText( "Latitude = " + lat + "º;\n" + "Longitude = " + lon + "º.");
+//*/
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
 
     }
 
-    public static class Installation {
-        private static String sID = null;
+    private class Installation {
+        private String sID = null;
         private static final String INSTALLATION = "INSTALLATION";
 
-        public synchronized static String id (Context context) {
+        synchronized String id(Context context) {
             if (sID == null) {
                 File installation = new File(context.getFilesDir(), INSTALLATION);
                 try {
@@ -107,7 +100,7 @@ https://developer.android.com/guide/topics/sensors/sensors_overview.html
             return sID;
         }
 
-        private static String readInstallationFile(File installation) throws IOException {
+        private String readInstallationFile(File installation) throws IOException {
             RandomAccessFile f = new RandomAccessFile(installation, "r");
             byte[] bytes = new byte[(int) f.length()];
             f.readFully(bytes);
@@ -115,11 +108,31 @@ https://developer.android.com/guide/topics/sensors/sensors_overview.html
             return new String(bytes);
         }
 
-        private static void writeInstallationFile(File installation) throws IOException {
+        // http://stackoverflow.com/questions/2885173/how-do-i-create-a-file-and-write-to-it-in-java
+        private void writeInstallationFile(File installation) throws IOException {
             FileOutputStream out = new FileOutputStream(installation);
+            //getSensorsData();
             String id = UUID.randomUUID().toString();
             out.write(id.getBytes());
             out.close();
+        }
+
+        private void getSensorsData() {
+        // Teste para printar na tela do app o num de sensores e algumas das suas infos. Para obter outras infos:
+        //https://developer.android.com/guide/topics/sensors/sensors_overview.html
+            // creates an instance (mSensorManager) to access sensors
+            SensorManager mSensorManager = ( SensorManager ) getSystemService( Context.SENSOR_SERVICE );
+            List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+            int nsensors = deviceSensors.size();
+            String[] name = new String[nsensors], vendor = new String[nsensors];
+            String[] it = new String[nsensors], version = new String[nsensors];
+
+            for (int i = 0; i < nsensors; i++){
+                name[i] = deviceSensors.get(i).getName();
+                vendor[i] = deviceSensors.get(i).getVendor();
+                it[i] = String.valueOf(i);
+                version[i] = String.valueOf(deviceSensors.get(i).getVersion());
+            }
         }
     }
 
@@ -129,6 +142,50 @@ https://developer.android.com/guide/topics/sensors/sensors_overview.html
         mLon = location.getLongitude();
         //TextView coord = (TextView) findViewById(R.id.Coords);
         //coord.setText( "Latitude = " + mLat + ";" + "Longitude = " + mLon );
+    }
+
+    private class MonitorTimer extends TimerTask {
+        @Override
+        public void run() {
+            DeadTimer mDeadTimer = new DeadTimer();
+            new Timer().schedule(mDeadTimer, 5000);
+            //double lastResulting = resulting;
+        }
+    }
+
+    // Creates a connection to http://labprodam.prefeitura.sp.gov.br/labfall/add
+    private class DeadTimer extends TimerTask {
+        @Override
+        public void run() {
+
+            final ConnectivityManager connectivityManager = ((ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE));
+
+            if (connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected()) {
+
+                try {
+                    URL url = new URL("http://labprodam.prefeitura.sp.gov.br/labfall/add");
+                    HttpURLConnection con = (HttpURLConnection) (url.openConnection());
+                    con.setRequestMethod("POST");
+                    con.setRequestProperty("USER-AGENT" , "Mozilla/5.0");
+                    con.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+                    con.setDoInput(true);
+                    con.setDoOutput(true);
+
+                    String data = "id=0&type=Alert&lat=" + mLat + "&lon=" + mLon;
+
+                    DataOutputStream os = new DataOutputStream(con.getOutputStream());
+                    os.writeBytes(data);
+                    os.flush();
+                    os.close();
+
+                    Log.e("FallResponse", "" + con.getResponseCode());
+
+                    con.disconnect();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     // Functions created when declared "implements SensorEventListener, LocationListener"
