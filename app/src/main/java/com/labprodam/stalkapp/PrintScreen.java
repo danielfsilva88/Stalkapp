81f342cfbdd48f774d3d881eeea773e75d558103
danielfsilva88 <daniel.ferreira.silva88@gmail.com>
2017-04-24 12:19:37 -0300

    printing-all-sensors-G5-using-ListView

--- app/src/main/java/com/labprodam/stalkapp/PrintScreen.java
+++ app/src/main/java/com/labprodam/stalkapp/PrintScreen.java
@@ -5,7 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
 
 public class PrintScreen extends AppCompatActivity {
 
@@ -15,17 +18,24 @@ public class PrintScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_print_screen);
+
         Log.i(TAG, "Return: you're here!");
 
         // "pega" o intent da Main
         Intent intentinfo = getIntent();
+
+        ListView listaSensores = (ListView) findViewById(R.id.lista);
+
         // armazena no array local (string[] infos) o array que foi passado pra ca
         // da Main, por meio da funcao putExtra (na main) - getStringArrayExtra (neste intent)
         String[] infos = intentinfo.getStringArrayExtra(MainActivity.EXTRA_MESSAGE);
 
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(infos[0] + "\n" + infos[1] + "\n" + infos[2] + "\n" + infos[3] + "\n"
-                       + infos[19] + "\n" + infos[20] + "\n" + infos[21] + "\n" + infos[22] + "\n");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, infos);
+        listaSensores.setAdapter(adapter);
+
+        //TextView textView = (TextView) findViewById(R.id.textView);
+        //textView.setText(infos[0] + "\n" + infos[1] + "\n" + infos[2] + "\n" + infos[3] + "\n"
+        //               + infos[19] + "\n" + infos[20] + "\n" + infos[21] + "\n" + infos[22] + "\n");
 
     }
 
